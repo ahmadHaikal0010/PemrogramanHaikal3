f4eddfd54d3dd6aa8add0be17972b84d37aad43e
ahmadHaikal0010 <haikala0010@gmail.com>
2024-09-02 11:00:32 +0700

    update konversi suhu

--- app/src/main/java/com/haikal/latihan5/MainActivity.kt
+++ app/src/main/java/com/haikal/latihan5/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.haikal.latihan5
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnKonversiSuhu : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        btnKonversiSuhu = findViewById(R.id.btnKonversiSuhu)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        btnKonversiSuhu.setOnClickListener() {
+            var intent = Intent(this@MainActivity, KonversiSuhuActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
